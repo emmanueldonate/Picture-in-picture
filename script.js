4a47084139c4ef683556b49d2ef9190c9999e127
Emmanuel <e.o.donate@gmail.com>
2020-07-18 14:26:56 -0700

    add video element

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const button = document.getElementById('button');
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
     try {
-        something
+       const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+       videoElement.srcObject = mediaStream;
+       videoElement.onloadedmetadata = () => {
+           videoElement.play();
+       }
     } catch (error) {
         // Catch Error here
         console.log('oops, error here:', error);
     }
 }
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
 
 // On load
 selectMediaStream();
