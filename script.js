5ecc626137e0261174413e18df0ed823ff0fff4f
Emmanuel <e.o.donate@gmail.com>
2020-07-18 14:07:06 -0700

    Script

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        something
+    } catch (error) {
+        // Catch Error here
+        console.log('oops, error here:', error);
+    }
+}
+
+// On load
+selectMediaStream();
